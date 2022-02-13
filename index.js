a4b196dab5379e684e5647ccf3736b822707e557
DEFIProtocol <92906608+DEFIProtocol@users.noreply.github.com>
2022-02-13 00:43:52 -0600

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-export {default as Navbar } from './Navbar';
-export {default as Cryptocurrencies } from './Cryptocurrencies';
-export {default as Homepage } from './Homepage';
-export {default as News } from './News';
-export { default as CryptoDetails } from './CryptoDetails';
-export { default as Login } from './Login';
-export { default as Register } from './Register';
-
